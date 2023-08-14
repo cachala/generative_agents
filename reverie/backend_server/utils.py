b4e36424ca009e5b1cd142fb6a03fa4706f270e0
cachala <104789086+cachala@users.noreply.github.com>
2023-08-14 21:54:19 +0800

    Create utils.py

---
+++ reverie/backend_server/utils.py
@@ -0,0 +1,16 @@
+# Copy and paste your OpenAI API Key
+openai_api_key = "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"
+# Put your name
+key_owner = "rica"
+
+maze_assets_loc = "../../environment/frontend_server/static_dirs/assets"
+env_matrix = f"{maze_assets_loc}/the_ville/matrix"
+env_visuals = f"{maze_assets_loc}/the_ville/visuals"
+
+fs_storage = "../../environment/frontend_server/storage"
+fs_temp_storage = "../../environment/frontend_server/temp_storage"
+
+collision_block_id = "32125"
+
+# Verbose 
+debug = True
